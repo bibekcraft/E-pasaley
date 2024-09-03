d882c5a6482be191e1c8dc8d165ea65bef1ff3df
Bibek1604 <npbibek94@gmail.com>
2024-09-03 12:37:01 +0545

    brand work addded

--- src/components/routing/FirstPage.tsx
+++ src/components/routing/FirstPage.tsx
@@ -1,11 +1,13 @@
 import First from '../firstpage/First'
 import Crausels from '../firstpage/Crausels'
 import TopArraival from '../firstpage/TopArraival'
+import WWW from '../firstpage/WWW'
 function FirstPage() {
   return (
     <><First />
     <Crausels />
     <TopArraival />
+    <WWW />
     </>
 
   )
