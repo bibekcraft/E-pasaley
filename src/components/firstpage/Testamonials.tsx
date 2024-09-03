5da6af4c6702c232e5bdedf1ff77582670fcaa48
Bibek1604 <npbibek94@gmail.com>
2024-09-03 14:22:48 +0545

    comment added

---
+++ src/components/firstpage/Testamonials.tsx
@@ -0,0 +1,47 @@
+
+function Testamonials() {
+  return (
+    <div><section className="px-4">
+    <div className="mx-auto max-w-3xl text-center">
+      <h3 className="mb-6 text-3xl font-bold">Testimonials</h3>
+      <p className="mb-6 pb-2 text-blue-700 md:mb-12">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit,
+        error amet numquam iure provident voluptate esse quasi, veritatis
+        totam voluptas nostrum quisquam eum porro a pariatur veniam.
+      </p>
+    </div>
+  
+    <div className="grid gap-12 text-center md:grid-cols-2">
+      {/* <!--First Testimonial--> */}
+      <div className="mb-6 md:mb-0">
+        <div className="mb-6 flex justify-center">
+          <img
+            src="https://tecdn.b-cdn.net/img/Photos/Avatars/img%20(22).jpg"
+            className="w-24 rounded-full shadow-lg dark:shadow-black/30" />
+        </div>
+        <p className="my-4 text-xl text-neutral-500 dark:text-blue-300">
+          "Lorem ipsum dolor sit amet eos adipisci, consectetur adipisicing
+          elit sed ut perspiciatis unde omnis."
+        </p>
+        <p className="italic">- Anna Morian</p>
+      </div>
+  
+      {/* <!--Second Testimonial--> */}
+      <div className="mb-0">
+        <div className="mb-6 flex justify-center">
+          <img
+            src="https://tecdn.b-cdn.net/img/Photos/Avatars/img%20(19).jpg"
+            className="w-24 rounded-full shadow-lg dark:shadow-black/30" />
+        </div>
+        <p className="my-4 text-xl text-neutral-500 dark:text-blue-300">
+          "Neque cupiditate assumenda in maiores repudiandae mollitia
+          architecto elit sed adipiscing elit."
+        </p>
+        <p className="italic">- Teresa May</p>
+      </div>
+    </div>
+  </section></div>
+  )
+}
+
+export default Testamonials
--- src/components/routing/FirstPage.tsx
+++ src/components/routing/FirstPage.tsx
@@ -2,12 +2,14 @@ import First from '../firstpage/First'
 import Crausels from '../firstpage/Crausels'
 import TopArraival from '../firstpage/TopArraival'
 import WWW from '../firstpage/WWW'
+import Testamonials from '../firstpage/Testamonials'
 function FirstPage() {
   return (
     <><First />
     <Crausels />
     <TopArraival />
     <WWW />
+    <Testamonials />
     </>
 
   )
